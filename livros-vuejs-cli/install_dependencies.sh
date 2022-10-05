eb6f6e7d65254022470d3da34913c2ad9b62ba5e
Paulo C. Silva Jr <pauluscave@gmail.com>
2022-10-05 19:45:35 -0300

    Adicionado dependência bulma e importado em projeto

--- livros-vuejs-cli/install_dependencies.sh
+++ livros-vuejs-cli/install_dependencies.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 npm i --save-dev @fortawesome/fontawesome-free
+npm install --save-dev node-sass sass-loader
+npm i --save bulma
--- livros-vuejs-cli/src/main.js
+++ livros-vuejs-cli/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'bulma'
+
 createApp(App).mount('#app')
