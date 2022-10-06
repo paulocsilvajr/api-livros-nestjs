984c5870b28928c58b8f542ba56c7f76d2b2b047
Paulo C. Silva Jr <pauluscave@gmail.com>
2022-10-06 20:37:44 -0300

    Removido componente HelloWorld, iniciado projeto com o mesmo código declarado no template de projeto .old

---
+++ livros-vuejs-cli/src/models/livro.ts
@@ -0,0 +1,8 @@
+export default class Livro {
+    id: number = 0
+    nome: string = ''
+    autor: string = ''
+    numeroPaginas: number = 0
+    dataCompra: Date = new Date()
+    lido: boolean = false
+} 
