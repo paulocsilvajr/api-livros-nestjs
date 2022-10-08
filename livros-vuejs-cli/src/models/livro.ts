8387327fc590cb3c03f1222146d803fa0ca13f44
Paulo C. Silva Jr <pauluscave@gmail.com>
2022-10-07 22:11:50 -0300

    Criado interface LivroJson para representar as propriedades do JSON oriundo de API e adicionado mais métodos à class Livro

--- livros-vuejs-cli/src/models/livro.ts
+++ livros-vuejs-cli/src/models/livro.ts
@@ -1,11 +1,38 @@
-export default class Livro {
+import LivroJson from "@/interfaces/livro-json"
 
+export default class Livro {
     constructor(
         public id: number = 0,
         public nome: string = '',
         public autor: string = '',
-        public numeroPaginas: number = 0,
+        public numeroPaginas: number = 1,
         public dataCompra: Date = new Date(),
         public lido: boolean = false
-        ) {}
+    ) { }
+
+    public set dataCompraInput(data: string) {
+        this.dataCompra = new Date(data)
+    }
+
+    public get dataCompraInput(): string {
+        try {
+            return this.dataCompra.toISOString().split('T')[0]
+        } catch (error) {
+            return ''
+        }
+    }
+
+    static fromJson(livro: LivroJson): Livro {
+        return new Livro(
+            livro.id,
+            livro.nome,
+            livro.autor,
+            livro.numeroPaginas,
+            new Date(livro.dataCompra),
+            livro.lido)
+    }
+
+    static toJson(livro: Livro): string {
+        return JSON.stringify(livro)
+    }
 }
