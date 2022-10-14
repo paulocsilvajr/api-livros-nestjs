7f5b5fd28fe786c4681f305b86d630a8af98df7b
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-10-14 11:01:17 -0300

    Adicionado morgan para gerar o log de requisições e respostas http de API. Adicionado em .env a porta da API e alterado hardcode em main.ts ref a porta API.

--- livros-api/install_dependencies.sh
+++ livros-api/install_dependencies.sh
@@ -1,2 +1,3 @@
-npm install --save @nestjs/typeorm typeorm pg
+npm i --save @nestjs/typeorm typeorm pg
 npm i --save @nestjs/config
+npm i --save morgan
--- livros-api/src/main.ts
+++ livros-api/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { log } from 'console';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { appendFile } from 'fs';
+// Fonte: https://stackoverflow.com/questions/55093055/logging-request-response-in-nest-js
+//        https://www.npmjs.com/package/morgan
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
+
+  await app.listen(process.env.PORTA_API, '0.0.0.0');
 }
-bootstrap();
+bootstrap().then(() => {
+  log(`API rodando em porta ${process.env.PORTA_API}`)
+});
