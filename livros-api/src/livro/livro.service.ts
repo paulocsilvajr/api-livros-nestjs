a1d4d0c14eac65cadb173abf2d7f84ccf3451f9d
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-11-29 15:00:04 -0300

    Adicionado postman collection atualizado

--- livros-api/src/livro/livro.service.ts
+++ livros-api/src/livro/livro.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { AlteraLivroDto, CadastraLivroDto } from "./livro.dto";
