058c71b9d8cf590c2f205854d4f1f85d42c2399d
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-12-14 09:09:30 -0300

    Adicionado e instalado pacotes para habilitar autenticação via JWT

--- livros-api/install_dependencies.sh
+++ livros-api/install_dependencies.sh
@@ -5,3 +5,5 @@ npm i --save class-transformer
 npm i --save class-validator
 npm i bcryptjs
 npm i --save-dev @types/bcryptjs
+npm i --save @nestjs/jwt @nestjs/passport passport-jwt passport-local
+npm i --save-dev @types/passport-local @types/passport-jwt
