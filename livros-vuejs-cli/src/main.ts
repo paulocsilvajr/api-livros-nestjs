9dabb68425e58411d8c9cb3f78b26410354ee061
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-12-14 15:22:15 -0300

    Adicionado vue-router via 'vue add router' e definido / como view CadastroLivrosView

--- livros-vuejs-cli/src/main.ts
+++ livros-vuejs-cli/src/main.ts
@@ -5,8 +5,10 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import 'bulma'
 
 import { store } from './store'
+import router from './router'
 
 const app = createApp(App)
 app.use(store)
+app.use(router)
 
 app.mount('#app')
---
+++ livros-vuejs-cli/src/views/CadastroLivrosView.vue
@@ -0,0 +1,14 @@
+<template>
+    <CadastroLivros />
+</template>
+
+<script lang="ts">
+import CadastroLivros from '../components/CadastroLivros.vue'
+
+export default {
+    name: "CadastroLivrosView",
+    components: {
+        CadastroLivros
+    }
+}
+</script>
