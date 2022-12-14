ade63edd388fcfee0c64d5805080a5abbfa7ac7b
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-12-14 10:54:45 -0300

    Adicionado dependência de morgan que faltava

--- livros-api/install_dependencies.sh
+++ livros-api/install_dependencies.sh
@@ -1,6 +1,7 @@
 npm i --save @nestjs/typeorm typeorm pg
 npm i --save @nestjs/config
 npm i --save morgan
+npm i --save-dev @types/morgan
 npm i --save class-transformer
 npm i --save class-validator
 npm i bcryptjs
