cb4f8ff9eb1a4e6e55063d8b9e23991f0b2a5431
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-12-15 14:13:03 -0300

    Adicionado template para EntrarComponent e adicionado div com class container em template de CadastroLivros

--- livros-vuejs-cli/src/components/CadastroLivrosComponent.vue
+++ livros-vuejs-cli/src/components/CadastroLivrosComponent.vue
@@ -1,154 +1,156 @@
 <template>
-
-    <div class="columns">
-        <div class="column mt-6">
-            <h1 class="title is-1 has-text-centered">{{ titulo }}</h1>
-        </div>
-    </div>
-
-    <div class="box">
+    <div class="container">
 
         <div class="columns">
-
-            <input type="text" id="id" hidden v-model="livro.id">
-
-            <div class="column is-three-quarters">
-                <div class="field">
-                    <label for="nome">Título</label>
-                    <div class="control">
-                        <input type="text" class="input" placeholder="Título do livro" id="nome"
-                            v-model.trim="livro.nome">
-                    </div>
-                    <p class="help is-danger" v-if="validacaoCampos.titulo">Informe um título</p>
-                </div>
-            </div>
-
-            <div class="column">
-                <div class="field">
-                    <label for="autor">Autor</label>
-                    <div class="control">
-                        <input type="text" class="input" placeholder="Nome do autor" id="autor"
-                            v-model.trim="livro.autor">
-                    </div>
-                    <p class="help is-danger" v-if="validacaoCampos.autor">Informe um autor</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="columns is-vcentered">
-
-            <div class="column">
-                <div class="field">
-                    <label for="npaginas">Número de páginas</label>
-                    <div class="control">
-                        <input type="number" min="1" class="input" placeholder="Número de páginas" id="npaginas"
-                            v-model="livro.numeroPaginas">
-                    </div>
-                    <p class="help is-danger" v-if="validacaoCampos.numeroPaginas">Informe um número de páginas válido
-                    </p>
-                </div>
-            </div>
-
-            <div class="column">
-                <div class="field">
-                    <label for="datacompra">Data de compra</label>
-                    <div class="control">
-                        <input type="date" class="input" placeholder="Data de compra" id="datacompra"
-                            v-model="livro.dataCompraInput">
-                    </div>
-                </div>
-            </div>
-
-            <div class="column is-one-quarter">
-                <div class="field">
-                    <div class="control">
-                        <label class="checkbox" for="lido">
-                            <input type="checkbox" id="lido" v-model="livro.lido">
-                            Lido
-                        </label>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="colums is-desktop">
-            <div class="column is-half">
-                <div class="columns">
-                    <div class="column">
-                        <button class="button is-success is-fullwidth" @click="salvaLivro">
-                            <span class="icon is-small">
-                                <i class="fas fa-check"></i>
-                            </span>
-                            <span>Salvar</span>
-                        </button>
-                    </div>
-                    <div class="column">
-                        <button class="button is-warning is-fullwidth" @click="defineLivroVazio">
-                            <span class="icon is-small">
-                                <i class="fas fa-times"></i>
-                            </span>
-                            <span>Limpar</span>
-                        </button>
-                    </div>
-                </div>
+            <div class="column mt-6">
+                <h1 class="title is-1 has-text-centered">{{ titulo }}</h1>
             </div>
         </div>
 
-    </div>
+        <div class="box">
 
-    <div class="notification is-danger" v-if="msg.length > 0">
-        {{ msg }}
-    </div>
+            <div class="columns">
 
+                <input type="text" id="id" hidden v-model="livro.id">
 
-    <div class="table-container mt-6">
-
-        <table class="table is-hoverable is-fullwidth">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Título</th>
-                    <th>Autor</th>
-                    <th>N° páginas</th>
-                    <th>Compra</th>
-                    <th>Lido</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tfoot>
-                <tr>
-                    <th>Total</th>
-                    <th>{{ totalLivros }}</th>
-                </tr>
-            </tfoot>
-            <tbody>
-                <tr v-for="l in livros" :key="l.id">
-                    <th>{{ l.id }}</th>
-                    <td>{{ l.nome }}</td>
-                    <td>{{ l.autor }}</td>
-                    <td>{{ l.numeroPaginas }}</td>
-                    <td>{{ formataDataBR(l.dataCompra) }}</td>
-                    <td>
-                        <strong v-if="l.lido">SIM</strong>
-                        <span v-else>não</span>
-                    </td>
-                    <td>
-                        <div class="buttons is-centered">
-                            <button class="button is-info is-small is-outlined"
-                                @click="alteraLivro(l.id)">Alterar</button>
-                            <button class="button is-danger is-small is-outlined"
-                                @click="removeLivro(l.id, l.nome)">Remover</button>
+                <div class="column is-three-quarters">
+                    <div class="field">
+                        <label for="nome">Título</label>
+                        <div class="control">
+                            <input type="text" class="input" placeholder="Título do livro" id="nome"
+                                v-model.trim="livro.nome">
                         </div>
-                    </td>
-                </tr>
+                        <p class="help is-danger" v-if="validacaoCampos.titulo">Informe um título</p>
+                    </div>
+                </div>
 
-            </tbody>
-        </table>
+                <div class="column">
+                    <div class="field">
+                        <label for="autor">Autor</label>
+                        <div class="control">
+                            <input type="text" class="input" placeholder="Nome do autor" id="autor"
+                                v-model.trim="livro.autor">
+                        </div>
+                        <p class="help is-danger" v-if="validacaoCampos.autor">Informe um autor</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="columns is-vcentered">
+
+                <div class="column">
+                    <div class="field">
+                        <label for="npaginas">Número de páginas</label>
+                        <div class="control">
+                            <input type="number" min="1" class="input" placeholder="Número de páginas" id="npaginas"
+                                v-model="livro.numeroPaginas">
+                        </div>
+                        <p class="help is-danger" v-if="validacaoCampos.numeroPaginas">Informe um número de páginas válido
+                        </p>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="field">
+                        <label for="datacompra">Data de compra</label>
+                        <div class="control">
+                            <input type="date" class="input" placeholder="Data de compra" id="datacompra"
+                                v-model="livro.dataCompraInput">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="column is-one-quarter">
+                    <div class="field">
+                        <div class="control">
+                            <label class="checkbox" for="lido">
+                                <input type="checkbox" id="lido" v-model="livro.lido">
+                                Lido
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="colums is-desktop">
+                <div class="column is-half">
+                    <div class="columns">
+                        <div class="column">
+                            <button class="button is-success is-fullwidth" @click="salvaLivro">
+                                <span class="icon is-small">
+                                    <i class="fas fa-check"></i>
+                                </span>
+                                <span>Salvar</span>
+                            </button>
+                        </div>
+                        <div class="column">
+                            <button class="button is-warning is-fullwidth" @click="defineLivroVazio">
+                                <span class="icon is-small">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                                <span>Limpar</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="notification is-danger" v-if="msg.length > 0">
+            {{ msg }}
+        </div>
+
+
+        <div class="table-container mt-6">
+
+            <table class="table is-hoverable is-fullwidth">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Título</th>
+                        <th>Autor</th>
+                        <th>N° páginas</th>
+                        <th>Compra</th>
+                        <th>Lido</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Total</th>
+                        <th>{{ totalLivros }}</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    <tr v-for="l in livros" :key="l.id">
+                        <th>{{ l.id }}</th>
+                        <td>{{ l.nome }}</td>
+                        <td>{{ l.autor }}</td>
+                        <td>{{ l.numeroPaginas }}</td>
+                        <td>{{ formataDataBR(l.dataCompra) }}</td>
+                        <td>
+                            <strong v-if="l.lido">SIM</strong>
+                            <span v-else>não</span>
+                        </td>
+                        <td>
+                            <div class="buttons is-centered">
+                                <button class="button is-info is-small is-outlined"
+                                    @click="alteraLivro(l.id)">Alterar</button>
+                                <button class="button is-danger is-small is-outlined"
+                                    @click="removeLivro(l.id, l.nome)">Remover</button>
+                            </div>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+        </div>
 
     </div>
-
 </template>
 
 <script lang="ts">
--- livros-vuejs-cli/src/components/EntrarComponent.vue
+++ livros-vuejs-cli/src/components/EntrarComponent.vue
@@ -1,9 +1,45 @@
 <template>
-    <h1>Login</h1>
+    <h1 class="title is-1 mt-3 mb-6">Login</h1>
+    
+    <div class="columns is-flex is-justify-content-center">
+        <div class="column is-6 is-4-desktop">
+            <div class="box">
+                <div class="field mb-5">
+                    <label for="usuario" class="label">Usuário</label>
+                    <div class="control">
+                        <input type="text" class="input" placeholder="Nome do usuário" id="usuario" v-model="usuario.nome">
+                    </div>
+                </div>
+                <div class="mb-6">
+                    <label for="senha" class="label">Senha</label>
+                    <div class="field has-addons">
+                        <div class="control is-expanded">
+                            <input type="password" class="input" placeholder="Senha do usuário" id="senha" ref="inputSenha" v-model="usuario.senha">
+                        </div>
+                        <div class="control">
+                            <button class="button" @click="alteraEstadoSenha()">
+                                <i class="fas" :class="{ 'fa-eye-slash': exibePassword, 'fa-eye': !exibePassword }"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="notification" :class="mensagem.tipo" v-if="mensagem.ativa">
+                    <button class="delete" @click="fechaNotificacao()"></button>
+                    {{ msgFormatada }}
+                </div>
+
+                <button class="button is-primary is-fullwidth mb-5" @click="entra()">Entrar</button>
+
+                <a href="/cadastro-usuario">Cadastrar um novo usuário</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Usuario from '@/models/usuario'
 
 
 export default defineComponent({
@@ -11,20 +47,46 @@ export default defineComponent({
     components: { },
     data() {
         return {
-            msg: '',
+            mensagem: {
+                texto: 'mensagem',
+                tipo: 'is-danger',
+                ativa: true 
+            },
+            exibePassword: true,
+            usuario: new Usuario(),
         }
     },
     computed: {
-        msgFormatada() {
+        msgFormatada(): string {
             // torna primeira letra maiúscula
-            return this.msg.charAt(0).toUpperCase() + this.msg.slice(1)
+            return this.mensagem.texto.charAt(0).toUpperCase() + this.mensagem.texto.slice(1);
         }
     },
     methods: {
+        alteraEstadoSenha() {
+            const inputSenha = this.$refs['inputSenha'] as any
+            if (inputSenha.type === 'password')
+                inputSenha.type = 'text';
+            else
+                inputSenha.type = 'password';
+            
+            this.exibePassword = !this.exibePassword;
+        },
+        entra() {
+            console.log(`Entrando em API com usuário '${this.usuario.nome}'...`);
 
+            this.limparCampos();
+        },
+        limparCampos() {
+            this.usuario.nome = '';
+            this.usuario.senha = '';
+        },
+        fechaNotificacao() {
+            this.mensagem.ativa = false;
+        }
     },
     beforeMount() {
-        console.log('método beforeMount')
+        console.log('método beforeMount');
     },
 })
 
