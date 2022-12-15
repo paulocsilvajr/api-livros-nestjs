6b6d7ebc0baa342ba8844286004ff588c93900da
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2022-12-15 15:25:17 -0300

    Removido vuex  - estava dando problema para reconhecer $store em component

--- livros-vuejs-cli/src/main.ts
+++ livros-vuejs-cli/src/main.ts
@@ -4,11 +4,9 @@ import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'bulma'
 
-import { store } from './store'
 import router from './router'
 
 const app = createApp(App)
-app.use(store)
 app.use(router)
 
 app.mount('#app')
