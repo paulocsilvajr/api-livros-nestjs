1dcd6ef53300f1e4e3636050ebfc942fcd4bddde
Paulo C. Silva Jr <pauluscave@gmail.com>
2022-12-27 13:52:34 -0300

    Corrigido lógica em InputSenha

--- livros-vuejs-cli/src/components/InputSenhaComponent.vue
+++ livros-vuejs-cli/src/components/InputSenhaComponent.vue
@@ -28,7 +28,7 @@ export default defineComponent({
     data() {
         return {
             content: this.value,
-            exibePassword: true,
+            exibePassword: false,
         }
     },
     methods: {
