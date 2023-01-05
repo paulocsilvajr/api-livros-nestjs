e3d9d1e2e35140dbd4a3862839c66770f0000405
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-01-05 09:11:21 -0300

    Refatorado funções axios para os services como classe e métodos

--- livros-vuejs-cli/src/services/index.ts
+++ livros-vuejs-cli/src/services/index.ts
@@ -1,45 +1,87 @@
-import axios from "axios"
+import axios, { AxiosInstance, AxiosResponse } from "axios"
 import VariaveisAmbiente from "@/utils/variaveis-ambiente"
 
-const ax = axios.create({
-    baseURL: `${VariaveisAmbiente.apiUrl}`,
-    headers: { 'Content-Type': 'application/json' },
-})
+class HttpAxiosService {
+    private ax: AxiosInstance
 
-const get = (url: string) => {
-    return ax.get(url)
+    constructor() {
+        this.ax = axios.create({
+            baseURL: `${VariaveisAmbiente.apiUrl}`,
+            headers: { 'Content-Type': 'application/json' },
+        })
+    }
+
+    public async get(url: string): Promise<AxiosResponse> {
+        return this.ax.get(url)
+            .then(response => {
+                return response
+            })
+            .catch(err => {
+                return err.response as AxiosResponse
+            });
+    }
+
+    public async getComParametros<P>(url: string, parametros: P): Promise<AxiosResponse> {
+        return this.ax.get(url, { params: parametros })
+            .then(response => {
+                return response
+            })
+            .catch(err => {
+                return err.response as AxiosResponse
+            });
+    }
+
+    public async getComToken(url: string, token: string): Promise<AxiosResponse> {
+        return this.ax.get(url, {
+            headers: this.headerToken(token)
+        })
+            .then(response => {
+                return response
+            })
+            .catch(err => {
+                return err.response as AxiosResponse
+            });
+    }
+
+    public async postComToken<P>(url: string, token: string, dados: P): Promise<AxiosResponse> {
+        return this.ax.post(url, JSON.stringify(dados), {
+            headers: this.headerToken(token)
+        })
+            .then(response => {
+                return response
+            })
+            .catch(err => {
+                return err.response as AxiosResponse
+            });
+    }
+
+    public async putComToken<P>(url: string, token: string, dados: P): Promise<AxiosResponse> {
+        return this.ax.put(url, JSON.stringify(dados), {
+            headers: this.headerToken(token)
+        })
+            .then(response => {
+                return response
+            })
+            .catch(err => {
+                return err.response as AxiosResponse
+            });
+    }
+
+    public async deleteComToken(url: string, token: string): Promise<AxiosResponse> {
+        return this.ax.delete(url, {
+            headers: this.headerToken(token)
+        })
+            .then(response => {
+                return response
+            })
+            .catch(err => {
+                return err.response as AxiosResponse
+            });
+    }
+
+    private headerToken(token: string) {
+        return { 'Authorization': `Bearer ${token}` }
+    }
 }
 
-const getComParametros = (url: string, parametros: any) => {
-    return ax.get(url, { params: parametros });
-}
-
-const headerToken = (token: string) => {
-    return { 'Authorization': `Bearer ${token}` }
-}
-
-const getComToken = (url: string, token: string) => {
-    return ax.get(url, {
-        headers: headerToken(token)
-    })
-}
-
-const postComToken = (url: string, token: string, dados: any) => {
-    return ax.post(url, JSON.stringify(dados), {
-        headers: headerToken(token)
-    })
-}
-
-const putComToken = (url: string, token: string, dados: any) => {
-    return ax.put(url, JSON.stringify(dados), {
-        headers: headerToken(token)
-    })
-}
-
-const deleteComToken = (url: string, token: string, dados: any) => {
-    return ax.delete(url, {
-        headers: headerToken(token)
-    })
-}
-
-export { get, getComParametros, getComToken, postComToken, putComToken, deleteComToken }
+export { HttpAxiosService }
