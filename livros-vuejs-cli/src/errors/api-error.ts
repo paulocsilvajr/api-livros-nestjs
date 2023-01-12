c0c7b2c31fed5c307d45e6900210eeb1fa28f90d
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-01-12 10:55:54 -0300

    Criado erro APIError para tratar error de API(response.data.message)

---
+++ livros-vuejs-cli/src/errors/api-error.ts
@@ -0,0 +1,8 @@
+import { AxiosResponse } from "axios";
+
+export class APIError extends Error {
+    constructor(resposta: AxiosResponse) {
+        
+        super(resposta.data.message.toString())
+    }
+}
