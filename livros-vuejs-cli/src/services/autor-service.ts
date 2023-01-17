cdd9d4e3a3d25e3fc016d72f9469f6237420daac
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-01-17 09:43:26 -0300

    Criado método salvaAutor em AutorService.

--- livros-vuejs-cli/src/services/autor-service.ts
+++ livros-vuejs-cli/src/services/autor-service.ts
@@ -1,4 +1,7 @@
+import { APIError } from "@/errors/api-error";
+import { CadastrarError } from "@/errors/cadastrar-error";
 import AutorJson from "@/interfaces/autor-json";
+import { Autor } from "@/models/autor";
 import { HttpAxiosService } from ".";
 
 export default class AutorService {
@@ -13,6 +16,22 @@ export default class AutorService {
             const data: AutorJson[] = response.data
 
             return data
+        }
+    }
+
+    public async salvaAutor(autor: Autor, token: string): Promise<AutorJson> {
+        const response = await this.axios.postComToken<Autor>(this.url, token, autor)
+
+        if (response.status === 201) {
+            const data: AutorJson = response.data
+
+            return data
+        } else if (response.status === 400) {
+            throw new APIError(response)
+        } else if (response.status === 406){
+            throw new CadastrarError('autor', 'Autor com nome informado já cadastrado')
+        } else {
+            throw new CadastrarError('autor')
         }
     }
 }
