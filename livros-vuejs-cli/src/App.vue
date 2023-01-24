e1dbf8ff76df6eac7a19415eb30410fbd693afab
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-01-24 16:23:14 -0300

    Comentado font-family em style - estava desalinhando verticalmente os textos na nav

--- livros-vuejs-cli/src/App.vue
+++ livros-vuejs-cli/src/App.vue
@@ -69,7 +69,7 @@ export default defineComponent({
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
