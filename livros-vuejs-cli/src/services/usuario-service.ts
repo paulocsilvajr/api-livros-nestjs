7bdb311c0136e0d42e3713c2a204f2806ef813ed
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-02-02 10:30:14 -0300

    Adicionado consulta por nome de usuário em UsuarioService

--- livros-vuejs-cli/src/services/usuario-service.ts
+++ livros-vuejs-cli/src/services/usuario-service.ts
@@ -1,5 +1,6 @@
 import { APIError } from "@/errors/api-error";
 import { CadastrarError } from "@/errors/cadastrar-error";
+import { NaoAutorizadoError } from "@/errors/nao-autorizado-error";
 import UsuarioJson from "@/interfaces/IUsuario";
 import { Usuario } from "@/models/usuario";
 import { HttpAxiosService } from ".";
@@ -21,6 +22,21 @@ export default class UsuarioService {
         } else {
             console.warn(response.data.message.toString())
             throw new CadastrarError('usuário')
+        }
+    }
+
+    public async buscaUsuarioPorNome(nome: string, token: string): Promise<UsuarioJson> {
+        const url = `${this.url}/${nome}`
+        const response = await this.axios.getComToken(url, token)
+
+        if (response.status === 200) {
+            const data: UsuarioJson = response.data
+
+            return data
+        } else if (response.status === 401) {
+            throw new NaoAutorizadoError()
+        } else {
+            throw new APIError(response)
         }
     }
 }
