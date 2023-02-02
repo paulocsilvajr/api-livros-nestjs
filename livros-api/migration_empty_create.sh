c589ed9386d90980eafa69a49264991f2cdf447f
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-02-02 16:35:02 -0300

    Em API, adicionado script para criar migration vazia - pode-se adicionar instruções SQL personalizadas

---
+++ livros-api/migration_empty_create.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Informe o nome da Migration com parâmetro"
+    exit 1
+fi
+
+parametros="$@"
+nome_migration="${parametros// /_}" # substituição de espaços( ) por underline(_)
+
+npm run typeorm -- migration:create ./migrations/$nome_migration
+
