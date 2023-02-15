59ad8f30f8011c119999eeb1ba827e1ec6be1ac8
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-02-15 08:14:34 -0300

    Em API, em LivroDisponivelService, adicionado função para de busca por ID e adicionado validação de src/util/verifica-entidade

--- livros-api/src/livro-disponivel/livro-disponivel.service.ts
+++ livros-api/src/livro-disponivel/livro-disponivel.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { InjectDataSource } from "@nestjs/typeorm";
+import { Livro } from "src/livro/livro.entity";
 import { DataSource } from "typeorm";
 import { LivroDisponivel } from "./livro-disponivel.entity";
+import { verificaArray, verificaNaoDisponivel } from "src/util/verifica-entidade";
 
 @Injectable()
 export class LivroDisponivelService {
@@ -22,6 +24,28 @@ export class LivroDisponivelService {
                 livro_disponivel;
         `)
 
+        verificaArray(livrosDisponiveisBanco, 'livros disponíveis')
+
         return livrosDisponiveisBanco
+    }
+
+    public async buscaLivroDisponivelPorId(id: number): Promise<Livro> {
+        const livroDisponivelBanco = await this.dataSource.query(`
+        select 
+            id,
+            titulo,
+            resumo,
+            "numeroPaginas",
+            "dataCompra",
+            "autorId"
+        from
+            livro_disponivel
+        where
+            id = ${id}
+        `)
+
+        verificaNaoDisponivel(livroDisponivelBanco, id, 'Livro');
+
+        return livroDisponivelBanco[0]
     }
 }
