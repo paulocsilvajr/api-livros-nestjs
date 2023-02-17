f5a5ce34548a55ecb7dbe6df2bbba7d45f351930
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-02-17 10:05:26 -0300

    Implementado guia Livros emprestados filtrando os livros entre emprestados e devolvidos

--- livros-vuejs-cli/src/components/EmprestimoLivrosComponent.vue
+++ livros-vuejs-cli/src/components/EmprestimoLivrosComponent.vue
@@ -9,27 +9,54 @@
             <template v-slot:guia01>
                 <div class="table-container mt-1">
 
-                    <table class="table is-hoverable is-fullwidth is-striped">
+                    <table class="table is-hoverable is-fullwidth is-striped mb-6">
                         <thead>
                             <tr>
-                                <th>A</th>
-                                <th>B</th>
-                                <th>C</th>
-                                <th></th>
+                                <th>Livro</th>
+                                <th>Autor</th>
+                                <th>Início de Leitura</th>
+                                <th>Fim da leitura</th>
                             </tr>
                         </thead>
                         <tfoot>
                             <tr>
                                 <th colspan="2">Total</th>
-                                <th colspan="2">???</th>
+                                <th colspan="3">{{ totalLivrosUsuarios }}</th>
                             </tr>
                         </tfoot>
                         <tbody>
+                            <tr v-for="lu in livrosUsuarios" :key="lu.id">
+                                <th class="is-vcentered">{{ lu.livroId }}</th>
+                                <td class="has-text-left is-vcentered">{{ lu.livroId }}</td>
+                                <td class="has-text-left is-vcentered">{{ formataData(lu.dataInicioLeitura) }}</td>
+                                <td class="has-text-left is-vcentered">{{ formataData(lu.dataFimLeitura) }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <h2 class="title is-2 has-text-left">Livros devolvidos</h2>
+
+                    <table class="table is-hoverable is-fullwidth is-striped">
+                        <thead>
                             <tr>
-                                <th class="is-vcentered">1</th>
-                                <td class="has-text-left is-vcentered">2</td>
-                                <td class="has-text-left is-vcentered">3</td>
-                                <td class="is-vcentered">4</td>
+                                <th>Livro</th>
+                                <th>Autor</th>
+                                <th>Início de Leitura</th>
+                                <th>Fim da leitura</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th colspan="2">Total</th>
+                                <th colspan="3">{{ totalLivrosUsuarios }}</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <tr v-for="lu in livrosUsuariosDevolvidos" :key="lu.id">
+                                <th class="is-vcentered">{{ lu.livroId }}</th>
+                                <td class="has-text-left is-vcentered">{{ lu.livroId }}</td>
+                                <td class="has-text-left is-vcentered">{{ formataData(lu.dataInicioLeitura) }}</td>
+                                <td class="has-text-left is-vcentered">{{ formataData(lu.dataFimLeitura) }}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -87,6 +114,9 @@ import LivroDisponivelService from '@/services/livro-disponivel'
 import LivroDisponivelJson from '@/interfaces/ILivroDisponivel';
 import AutorService from '@/services/autor-service';
 import AutorJson from '@/interfaces/IAutor';
+import LivroUsuarioService from '@/services/livro-usuario-service';
+import LivroUsuarioJson from '@/interfaces/ILivroUsuario';
+import { formataDataBR } from '@/utils/formataData';
 
 export default defineComponent({
     name: 'EmprestimoLivrosComponent',
@@ -100,11 +130,18 @@ export default defineComponent({
             livroDisponivelService: new LivroDisponivelService(),
             autorService: new AutorService(),
             autores: [] as AutorJson[],
+            livrosUsuarios: [] as LivroUsuarioJson[],
+            livroUsuarioService: new LivroUsuarioService(),
+            livrosUsuariosDevolvidos: [] as LivroUsuarioJson[],
         }
     },
     computed: {
         totalLivrosDisponiveis(): string {
             const tamanho = this.livrosDisponiveis.length
+            return `${tamanho} livro${tamanho != 1 ? "s" : ""}`
+        },
+        totalLivrosUsuarios(): string {
+            const tamanho = this.livrosUsuarios.length
             return `${tamanho} livro${tamanho != 1 ? "s" : ""}`
         }
     },
@@ -126,10 +163,34 @@ export default defineComponent({
                 this.autores = autoresBanco
             }
         },
+        async buscaLivrosUsuarios() {
+            console.log("Buscando livros emprestados a usuários em API")
+            const livrosUsuariosBanco = await this.livroUsuarioService.buscaLivroUsuarioPorUsuario(this.token, this.usuario)
+
+            if (livrosUsuariosBanco) {
+                console.log("Listando livros emprestados e devolvidos")
+                livrosUsuariosBanco.forEach((lub) => {
+                    if (!lub.dataFimLeitura) {
+                        this.livrosUsuarios.push(lub)
+                    } else {
+                        this.livrosUsuariosDevolvidos.push(lub)
+                    }
+                })
+            }
+        },
         retornaNomeAutor(id: number) {
             const autorEncontrado = this.autores.find(autor => autor.id === id)
             return autorEncontrado?.nome
         },
+        formataData(data: string): string {
+            if (!data) {
+                return ""
+            }
+
+            const dataConvertida = new Date(data)
+
+            return formataDataBR(dataConvertida)
+        }
     },
     beforeMount() {
         if (this.semToken) {
@@ -140,11 +201,13 @@ export default defineComponent({
 
         this.buscaAutores()
         this.buscaLivrosDisponiveis()
+        this.buscaLivrosUsuarios()
     },
     setup() {
         const store = useStore()
         const semToken = computed(() => store.getters.semToken)
         const token = computed(() => store.state.usuario.token)
+        const usuario = computed(() => store.state.usuario.nomeUsuario)
 
         const { notificar } = useNotificador()
         const { definirGuiaAtiva } = useDefinidorGuiaAtiva()
@@ -153,7 +216,8 @@ export default defineComponent({
             semToken,
             notificar,
             token,
-            definirGuiaAtiva
+            definirGuiaAtiva,
+            usuario,
         }
     }
 })
