5b442d3948e9b3582638a98cf9334109414aaece
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-03-08 11:31:56 -0300

    Adicionado interface LivroUsuarioCadastro. Criado service salvaLivroUsuario para salvar um livro associado a um usuário no BD

--- livros-vuejs-cli/src/interfaces/ILivroUsuario.ts
+++ livros-vuejs-cli/src/interfaces/ILivroUsuario.ts
@@ -1,7 +1,16 @@
-export default interface LivroUsuarioJson {
+interface LivroUsuarioJson {
     id: number,
     usuarioNome: string,
     livroId: number,
     dataInicioLeitura: string,
     dataFimLeitura: string
 }
+
+interface LivroUsuarioCadastro {
+    livro: number,
+    usuario: string,
+    dataInicioLeitura: string,
+    dataFimLeitura?: string
+}
+
+export { LivroUsuarioJson, LivroUsuarioCadastro }
--- livros-vuejs-cli/src/services/livro-usuario-service.ts
+++ livros-vuejs-cli/src/services/livro-usuario-service.ts
@@ -1,4 +1,5 @@
-import LivroUsuarioJson from "@/interfaces/ILivroUsuario";
+import { APIError } from "@/errors/api-error";
+import { LivroUsuarioJson, LivroUsuarioCadastro } from "@/interfaces/ILivroUsuario"
 import { HttpAxiosService } from ".";
 
 export default class LivroUsuarioService {
@@ -15,6 +16,26 @@ export default class LivroUsuarioService {
             const data: LivroUsuarioJson[] = response.data
 
             return data
+        }
+    }
+
+    public async salvaLivroUsuario(token: string, idLivro: number, usuario: string): Promise<LivroUsuarioJson | undefined> {
+        const livroUsuarioCadastro: LivroUsuarioCadastro = {
+            livro: idLivro,
+            usuario: usuario,
+            dataInicioLeitura: new Date().toISOString()
+        }
+
+        const url = `${this.url}`
+
+        const response = await this.axios.postComToken(url, token, livroUsuarioCadastro)
+
+        if (response.status === 201) {
+            const data: LivroUsuarioJson = response.data
+
+            return data
+        } else {
+            throw new APIError(response)
         }
     }
 }
